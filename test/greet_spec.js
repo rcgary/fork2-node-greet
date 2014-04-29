7a4b8e9df91b323e1dc1e2f8cb93bface13fc366
Chao Ruan <chaoruan818@gmail.com>
2014-04-29 14:37:30 +1000

    add test file

---
+++ test/greet_spec.js
@@ -0,0 +1,12 @@
+var greet = require("../src/index")
+  , name = "Chao"
+  , drunkSentence = ", you look sexy today";
+
+describe('greet',function(){
+  it("should greet a preson by name",function(){
+    expect("hello, " + name).to.eql(greet(name));
+  });
+  it("should greet a preson by flitatiously if drunk",function(){
+    expect("hello, " + name + drunkSentence).to.eql(greet(name,true));
+  });
+});
